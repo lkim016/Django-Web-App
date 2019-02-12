c183c1c67ee0c3a98d8bd4e22da47cdd83538855
Lori Kim <kim.lori57@gmail.com>
2019-02-12 17:43:21 -0500

    REMOVED UNNEEDED COMMENTS

--- alka/views.py
+++ alka/views.py
@@ -36,19 +36,3 @@ def input(request):
 
         return render(request, "alka/input.html", {"in_form": info_form, "add_form": address_form})
 
-#def home(request):
-    #info_form = CustomerInfoForm(request.POST or None)
-    #address_form = DeliveryAddressForm(request.POST or None)
-    #deposit_form = DepositInfoForm(request.POST or None)
-
-    #if request.method == "POST":
-        #if info_form.is_valid(): #and address_form.is_valid() and deposit_form.is_valid():
-            #customer = info_form.save(commit=False) # commit=False tells Django: "Don't send this to database yet.
-            #info.save()
-            #return redirect("home")
-
-            # need to check the input of the form to see if the form is put into an object so that I can combine the 3 form inputs on 'POST'
-            #customer = info_form.cleaned_data['name']
-            #return render(request, "alka/home.html", {"result": customer})
-    #else:
-        #return render(request, "alka/home.html", {"in_form": info_form, "add_form": address_form, "dep_form": deposit_form})
